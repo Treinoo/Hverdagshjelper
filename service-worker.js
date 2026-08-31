57eb2465a9db144729538c1d20ed268646949168
Treinoo <trine.espeland@frydregnskap.no>
2026-08-31 21:31:17 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,11 @@
-const CACHE='matvenn-full-v6';
-const ASSETS=['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener("install", event => self.skipWaiting());
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request));
+});
